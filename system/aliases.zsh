342dcfd7122bba6deb494252188da177b312ce2c
Jay Chung <ken8203@gmail.com>
2021-08-23 11:56:48 +0800

    feat: clean up

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -5,6 +5,7 @@ alias cat='bat'
 alias mkdir='mkdir -p'
 alias kc="kubectl"
 alias reload="source $HOME/.zshrc && exec $SHELL -l"
+alias plantuml="docker run -d -p 8080:8080 plantuml/plantuml-server:jetty"
 
 # Include custom aliases
 if [[ -f ~/.aliases.local ]]; then
