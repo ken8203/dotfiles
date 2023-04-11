6ba2d98e9eb9b75c9ab358c560635406b77b4080
Jay Chung <ken8203@gmail.com>
2023-04-11 15:38:46 +0800

    feat: add offlinepg

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -6,6 +6,7 @@ alias mkdir='mkdir -p'
 alias kc="kubectl"
 alias reload="source $HOME/.zshrc && exec $SHELL -l"
 alias plantuml="docker run -d -p 8080:8080 plantuml/plantuml-server:jetty"
+alias offlinepg="gcloud compute ssh --zone asia-east1-a portal-os-login-1 --project dcard-production --tunnel-through-iap -- -L 127.0.0.1:5431:172.20.15.15:5432"
 
 # Include custom aliases
 if [[ -f ~/.aliases.local ]]; then
