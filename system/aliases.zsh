d153410a9718fce37825f7c5a70d097834829054
Jay Chung <ken8203@gmail.com>
2024-03-07 18:17:28 +0800

    feat: kcexec

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -9,6 +9,10 @@ alias plantuml="docker run -d -p 8080:8080 plantuml/plantuml-server:jetty"
 alias offlinepg="gcloud compute ssh --zone asia-east1-a portal-os-login-1 --project dcard-production --tunnel-through-iap -- -L 127.0.0.1:5431:172.20.15.15:5432"
 alias gal="gcloud auth login --update-adc"
 
+kcexec() {
+  kubectl exec -it $1 -- sh
+}
+
 # Include custom aliases
 if [[ -f ~/.aliases.local ]]; then
   source ~/.aliases.local
