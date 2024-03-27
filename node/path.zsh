031d4f38e66f8c5bd86b47effc43ba998645e190
Jay Chung <ken8203@gmail.com>
2024-03-27 10:31:00 +0800

    feat(node): use v18

--- node/path.zsh
+++ node/path.zsh
@@ -1,4 +1,4 @@
-export PATH="/opt/homebrew/opt/node@16/bin:$PATH"
-export LDFLAGS="-L/opt/homebrew/opt/node@14/li6"
-export CPPFLAGS="-I/opt/homebrew/opt/node@16/include"
+export PATH="/opt/homebrew/opt/node@18/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/node@18/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/node@18/include"
 export PATH="$(yarn global bin):$PATH"
