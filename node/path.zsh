40f73db7a061946bea5c3891b54306d759316773
Jay Chung <ken8203@gmail.com>
2024-07-26 14:57:26 +0800

    feat(node): use v20

--- node/path.zsh
+++ node/path.zsh
@@ -1,4 +1,4 @@
-export PATH="/opt/homebrew/opt/node@18/bin:$PATH"
-export LDFLAGS="-L/opt/homebrew/opt/node@18/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/node@18/include"
+export PATH="/opt/homebrew/opt/node@20/bin:$PATH"
+export LDFLAGS="-L/opt/homebrew/opt/node@20/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/node@20/include"
 export PATH="$(yarn global bin):$PATH"
